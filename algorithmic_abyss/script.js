d71e515e3c8d98d4999136fe7f4526b2df494ba3
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-12-17 11:09:54 +0100

    new algorithms and names

--- algorithmic_abyss/script.js
+++ algorithmic_abyss/script.js
@@ -1,3 +1,9 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function StringBuilder(value) {
     this.strings = new Array()
     this.append(value)
@@ -76,27 +82,39 @@ class Rules {
     birthRule
     survivalRule
     initialFillPercent
+    initialFillCount
+    initialFillSquare
     wrap
 
-    constructor(birthRule, survivalRule, initialFillPercent, wrap) {
+    constructor(birthRule, survivalRule, initialFillPercent = 0.0, initialFillCount = 2, wrap, initialFillSquare = false) {
         this.birthRule = birthRule
         this.survivalRule = survivalRule
         this.initialFillPercent = initialFillPercent
+        this.initialFillCount = initialFillCount
+        this.initialFillSquare = initialFillSquare
         this.wrap = wrap
     }
 }
 
-const GAME_OF_LIFE = new Rules([3], [2, 3], 12, false)
-const INEVITABLE_EXPANSION = new Rules([3, 4], [3, 4], 10)
-const DIAMOEBA = new Rules([3, 5, 6, 7, 8], [5, 6, 7, 8], 51, false)
-const DAY_AND_NIGHT = new Rules([3, 6, 7, 8], [3, 4, 6, 7, 8], 50, true)
-const ANNEAL = new Rules([4, 6, 7, 8], [3, 5, 6, 7, 8], 48, true)
+const AI_CORRUPTION = new Rules([2, 3, 4], [1, 2], initialFillPercent=0.0, initialFillCount=2, wrap=true, initialFillSquare=true)
+const CRYSTAL_MAZE = new Rules([1], [1, 2, 3, 4], initialFillPercent=0, initialFillCount=2, wrap=true)
+const LAVA = new Rules([3, 4], [2, 3, 4], initialFillPercent=4, initialFillCount=0, wrap=false)
+const GAME_OF_LIFE = new Rules([3], [2, 3], initialFillPercent=12, initialFillCount=0, wrap=false)
+const INEVITABLE_EXPANSION = new Rules([3, 4], [3, 4], initialFillPercent=10, initialFillCount=0, wrap=false)
+const DIAMOEBA = new Rules([3, 5, 6, 7, 8], [5, 6, 7, 8], initialFillPercent=51, initialFillCount=0, wrap=false)
+const DAY_AND_NIGHT = new Rules([3, 6, 7, 8], [3, 4, 6, 7, 8], initialFillPercent=50, initialFillCount=0, wrap=true)
+const ANNEAL = new Rules([4, 6, 7, 8], [3, 5, 6, 7, 8], initialFillPercent=48, initialFillCount=0, wrap=true)
 
-const rulesArray = [INEVITABLE_EXPANSION, DIAMOEBA, DAY_AND_NIGHT];
+const DEBUG_TEST_RULE = false
+const TEST_RULE = GAME_OF_LIFE
+
+const rulesArray = [LAVA, INEVITABLE_EXPANSION, DIAMOEBA, DAY_AND_NIGHT, AI_CORRUPTION, CRYSTAL_MAZE];
 const randomRulesIndex = Math.floor(Math.random() * rulesArray.length)
-const RULES = rulesArray[randomRulesIndex];
+const RULES = DEBUG_TEST_RULE ? TEST_RULE : rulesArray[randomRulesIndex];
 
-if (RULES == GAME_OF_LIFE) {
+if (RULES == TEST_RULE) {
+    tuiHead.innerText += " TEST"
+} else if (RULES == GAME_OF_LIFE) {
     tuiHead.innerText += " GAME OF LIFE"
 } else if (RULES == INEVITABLE_EXPANSION) {
     tuiHead.innerText += " INEVITABLE EXPANSION"
@@ -106,6 +124,12 @@ if (RULES == GAME_OF_LIFE) {
     tuiHead.innerText += " CONTINENTS BIRTH"
 } else if (RULES == ANNEAL) {
     tuiHead.innerText += " ANNEAL"
+} else if (RULES == LAVA) {
+    tuiHead.innerText += " LAVA"
+} else if (RULES == CRYSTAL_MAZE) {
+    tuiHead.innerText += " CRYSTAL MAZE"
+} else if (RULES == AI_CORRUPTION) {
+    tuiHead.innerText += " AI CORRUPTION"
 }
 
 function update(old)
@@ -147,6 +171,22 @@ for (let r = 0; r < HEIGHT; ++r) {
     world.push([])
     for (let c = 0; c < WIDTH; ++c) {
         world[r].push(Math.random() <= (RULES.initialFillPercent / 100))
+    }
+}
+
+if (RULES.initialFillPercent == 0) {
+    for (let i = 0; i<RULES.initialFillCount; ++i) {
+        if (RULES.initialFillSquare) {
+            let y = getRandomInt(0, HEIGHT-1)
+            let x = getRandomInt(0, WIDTH-1)
+
+            world[y][x] = true
+            world[(y + 1 + WIDTH) % WIDTH][(x + 1 + WIDTH) % WIDTH] = true
+            world[(y + 1 + WIDTH) % WIDTH][x] = true
+            world[y][(x + 1 + WIDTH) % WIDTH] = true
+        } else {
+            world[getRandomInt(0, HEIGHT-1)][getRandomInt(0, WIDTH-1)] = true
+        }
     }
 }
 
