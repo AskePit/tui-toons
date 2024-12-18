22c487f9297b33cc9ea51c1ef7270a4138ac2b49
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-12-18 22:01:08 +0100

    matrix class

--- raytracer/raytracer.js
+++ raytracer/raytracer.js
@@ -58,12 +58,12 @@ class Vec3 {
         return this
     }
 
-    length_squared() {
+    lengthSquared() {
         return this.x*this.x + this.y*this.y + this.z*this.z
     }
 
     length() {
-        return Math.sqrt(this.length_squared())
+        return Math.sqrt(this.lengthSquared())
     }
 
     normalize() {
@@ -92,6 +92,167 @@ function cross(v1, v2) {
        -v1.x*v2.z - v1.z*v2.x,
         v1.x*v2.y - v1.y*v2.x
     )
+}
+
+class Matrix4x4 {
+    elements // Float32Array(16)
+
+    constructor() {
+        this.elements = new Float32Array(16)
+        this.identity()
+    }
+
+    identity() {
+        const e = this.elements
+        e.fill(0)
+        e[0] = e[5] = e[10] = e[15] = 1
+        return this
+    }
+
+    translate(x, y, z) {
+        const e = this.elements
+        e[12] += x
+        e[13] += y
+        e[14] += z
+        return this
+    }
+
+    scale(x, y, z) {
+        const e = this.elements
+        e[0] *= x
+        e[5] *= y
+        e[10] *= z
+        return this
+    }
+
+    rotateX(angle) {
+        const e = this.elements
+        const c = Math.cos(angle)
+        const s = Math.sin(angle)
+
+        const m10 = e[4], m11 = e[5], m12 = e[6], m13 = e[7]
+        const m20 = e[8], m21 = e[9], m22 = e[10], m23 = e[11]
+
+        e[4] = c * m10 + s * m20
+        e[5] = c * m11 + s * m21
+        e[6] = c * m12 + s * m22
+        e[7] = c * m13 + s * m23
+
+        e[8] = c * m20 - s * m10
+        e[9] = c * m21 - s * m11
+        e[10] = c * m22 - s * m12
+        e[11] = c * m23 - s * m13
+
+        return this
+    }
+
+    rotateY(angle) {
+        const e = this.elements
+        const c = Math.cos(angle)
+        const s = Math.sin(angle)
+
+        const m00 = e[0], m01 = e[1], m02 = e[2], m03 = e[3]
+        const m20 = e[8], m21 = e[9], m22 = e[10], m23 = e[11]
+
+        e[0] = c * m00 - s * m20
+        e[1] = c * m01 - s * m21
+        e[2] = c * m02 - s * m22
+        e[3] = c * m03 - s * m23
+
+        e[8] = c * m20 + s * m00
+        e[9] = c * m21 + s * m01
+        e[10] = c * m22 + s * m02
+        e[11] = c * m23 + s * m03
+
+        return this
+    }
+
+    rotateZ(angle) {
+        const e = this.elements
+        const c = Math.cos(angle)
+        const s = Math.sin(angle)
+
+        const m00 = e[0], m01 = e[1], m02 = e[2], m03 = e[3]
+        const m10 = e[4], m11 = e[5], m12 = e[6], m13 = e[7]
+
+        e[0] = c * m00 + s * m10
+        e[1] = c * m01 + s * m11
+        e[2] = c * m02 + s * m12
+        e[3] = c * m03 + s * m13
+
+        e[4] = c * m10 - s * m00
+        e[5] = c * m11 - s * m01
+        e[6] = c * m12 - s * m02
+        e[7] = c * m13 - s * m03
+
+        return this
+    }
+
+    multiply(other) {
+        const a = this.elements
+        const b = other.elements
+        const result = new Float32Array(16)
+
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                result[i + j * 4] =
+                    a[i] * b[j * 4] +
+                    a[i + 4] * b[j * 4 + 1] +
+                    a[i + 8] * b[j * 4 + 2] +
+                    a[i + 12] * b[j * 4 + 3]
+            }
+        }
+
+        this.elements = result
+        return this
+    }
+
+    applyToVector(vec) {
+        const e = this.elements
+        const x = vec.x, y = vec.y, z = vec.z
+        const w = 1.0
+
+        return new Vec3(
+            e[0] * x + e[4] * y + e[8] * z + e[12] * w,
+            e[1] * x + e[5] * y + e[9] * z + e[13] * w,
+            e[2] * x + e[6] * y + e[10] * z + e[14] * w
+        )
+    }
+
+    lookAt(eye, target, up) {
+        const zAxis = eye.clone().sub(target).normalize()
+        const xAxis = up.clone().cross(zAxis).normalize()
+        const yAxis = zAxis.clone().cross(xAxis)
+
+        const e = this.elements
+        e[0] = xAxis.x
+        e[1] = yAxis.x
+        e[2] = zAxis.x
+        e[3] = 0
+
+        e[4] = xAxis.y
+        e[5] = yAxis.y
+        e[6] = zAxis.y
+        e[7] = 0
+
+        e[8] = xAxis.z
+        e[9] = yAxis.z
+        e[10] = zAxis.z
+        e[11] = 0
+
+        e[12] = -xAxis.dot(eye)
+        e[13] = -yAxis.dot(eye)
+        e[14] = -zAxis.dot(eye)
+        e[15] = 1
+
+        return this
+    }
+
+    clone() {
+        const result = new Matrix4x4()
+        result.elements.set(this.elements)
+        return result
+    }
 }
 
 class Ray {
