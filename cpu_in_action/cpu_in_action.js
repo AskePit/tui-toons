69947bc4badacac6062abb8659764d0f91e06244
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2025-08-20 10:39:41 +0200

    identify diagram top-left locations for main blocks

--- cpu_in_action/cpu_in_action.js
+++ cpu_in_action/cpu_in_action.js
@@ -1,7 +1,43 @@
 setTuiMode(TuiMode.TEXT)
 
-tui.textContent = `                              
-                                        ┌──────────────────────────────────────────┐                                   
+const Loc =
+{
+    cpu1 : {
+        cache : {
+            TL: 1324,
+        },
+        read : {
+            TL: 744,
+        },
+        write : {
+            TL: 1944,
+        },
+        code : {
+            TL: 3364,
+        },
+    },
+    cpu2 : {
+        cache : {
+            TL: 1422,
+        },
+        read : {
+            TL: 802,
+        },
+        write : {
+            TL: 2002,
+        },
+        code : {
+            TL: 3442,
+        },
+    },
+    memory : {
+        TL: 769,
+    }
+}
+
+
+let screen =
+`                                        ┌──────────────────────────────────────────┐                                   
                                         │                                          │                                   
                                         │                                          │                                   
                       ┌──────────────┐  │  /  ┌────────────────────────┐  /     ┌──▼───────────┐                       
@@ -46,3 +82,17 @@ tui.textContent = `
                                            /                              /                                            
                                            /                              /                                            
 `;
+
+function setCharAt(str, index, chr) {
+  if (index < 0 || index >= str.length) return str; // out of range
+  return str.substring(0, index) + chr + str.substring(index + 1);
+}
+
+for (const cpu of [Loc.cpu1, Loc.cpu2]) {
+    for (const obj of [cpu.cache, cpu.read, cpu.write, cpu.code]) {
+        screen = setCharAt(screen, obj.TL, '@')
+    }
+    screen = setCharAt(screen, Loc.memory.TL, '@')
+}
+
+tui.textContent = screen
